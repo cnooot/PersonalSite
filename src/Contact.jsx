b42921f8bbe027729ac6deb70e2eaa0efae2b864
cnooot <musiekowaty@gmail.com>
2024-01-10 06:57:50 +0100

    added working getintouch form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-
+import emailjs from "@emailjs/browser";
 
 function Contact(){
 
+    const sendEmail = (e)=>{
+        e.preventDefault();
+        alert("submitted");
+        emailjs.sendForm('service_yd6ktyg', 'template_b4vcked', e.target, 'w0ta3B_1ZxcmAOvPS')
+    };
     return(   
         <div className="Tit">
             <div className="projectstitle">
                 <h2>Get in touch!</h2>
             </div>
+            <form className="contactform" onSubmit={sendEmail}>
             <div className="contactbox">
                 <div className="topcontact">
-                    <input type="text" className="name" placeholder="Your Name" />  
-                    <input type="text" className="phone" placeholder="Your phone number"/>
-                    <input type="text" className="email" placeholder="Your email"/>
+                    <input type="text" id="name" name="name" className="name" placeholder="Your Name" required/>  
+                    <input type="text" id="phone" className="phone" placeholder="Your phone number" required/>
+                    <input type="email" id="email" className="email" placeholder="Your email" required/>
                 </div>
                 <div className="botcontact">
                     <textarea name="" id="txtarea" cols="30" rows="10" placeholder="Leave ur message here!"></textarea>
@@ -21,6 +27,7 @@ function Contact(){
                     </div>
                 </div>
             </div>
+            </form>
         </div>
 
 
